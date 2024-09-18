e81f59de0240b40ebf5f45efc528803ed042da5b
HP <HP@DESKTOP-98D1A1C>
2024-09-18 14:12:49 +0530

    Add project files.

---
+++ User/Login.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineJobPortal.User
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+            }
+            catch(Exception ex)  
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+    }
+}
